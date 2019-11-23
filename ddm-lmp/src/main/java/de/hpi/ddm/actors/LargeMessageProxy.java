daee749773e6cab16b431c644dc5bcf426f64e06
Tibor Zequini Boglár <tibor.zequini@gmail.com>
2019-11-23 07:02:05 +0100

    applied small fix

--- ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
+++ ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
@@ -32,93 +32,6 @@ public class LargeMessageProxy extends AbstractLoggingActor {
     // Actor Messages //
     ////////////////////
 
-    @Override
-    public Receive createReceive() {
-        return receiveBuilder()
-                .match(LargeMessage.class, this::handle)
-                .match(BytesMessage.class, this::handle)
-                .match(SourceMessage.class, this::handle)
-                .match(SerializedByteMessage.class, this::deserializer)
-                .matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
-                .build();
-    }
-
-    private void handle(LargeMessage<?> message) {
-        ActorRef receiver = message.getReceiver();
-        ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
-
-        // Serializing the message
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        Kryo kryo = new Kryo();
-        Output output = new Output(byteArrayOutputStream);
-        kryo.writeClassAndObject(output, message.getMessage());
-        output.close();
-        byte[] byteArrayData = byteArrayOutputStream.toByteArray();
-
-        // Akka Streaming
-        Source<List<Byte>, NotUsed> source = Source.from(Arrays.asList(ArrayUtils.toObject(byteArrayData))).grouped(262144);
-        SourceRef<List<Byte>> sourceRef = source.runWith(StreamRefs.sourceRef(), this.context().system());
-
-        // Passing the source reference as a customized "SourceMessage"
-        receiverProxy.tell(new SourceMessage(sourceRef, byteArrayData.length, this.sender(), message.getReceiver()), this.self());
-    }
-
-    private void handle(SourceMessage message) {
-        ActorRef receiver = message.getReceiver();
-        ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
-
-        // Receiving the customized "SourceMessage" and retrieving the source reference
-        SourceRef<List<Byte>> sourceRef = message.getSourceRef();
-        byte[] bytes = new byte[message.getLength()];
-        sourceRef.getSource().runWith(Sink.seq(), this.context().system()).whenComplete
-                (
-                        (data, exception) -> {
-                            int index = 0;
-                            for (List<Byte> list : data) {
-                                for (Byte abyte : list) {
-                                    bytes[index] = abyte;
-                                    index++;
-                                }
-                            }
-
-                            SerializedByteMessage serializedByteMessage = new SerializedByteMessage(bytes, message.getReceiver(), message.getSender());
-                            receiverProxy.tell(serializedByteMessage, message.getSender());
-                        }
-                );
-    }
-
-    private void handle(BytesMessage<?> message) {
-        // Reassemble the message content, deserialize it and/or load the content from some local location before forwarding its content.
-        message.getReceiver().tell(message.getBytes(), message.getSender());
-    }
-
-    /////////////////
-    // Actor State //
-    /////////////////
-
-    /////////////////////
-    // Actor Lifecycle //
-    /////////////////////
-
-    ////////////////////
-    // Actor Behavior //
-    ////////////////////
-
-    private void deserializer(SerializedByteMessage<?> message) {
-        try {
-            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(message.bytes);
-            Kryo kryo = new Kryo();
-            Input input = new Input(byteArrayInputStream);
-            Object finalData = kryo.readClassAndObject(input);
-            input.close();
-            byteArrayInputStream.close();
-
-            // Finally, we send the deserialize object to its destination
-            message.receiver.tell(finalData, message.sender);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     @Data
     @NoArgsConstructor
@@ -142,23 +55,99 @@ public class LargeMessageProxy extends AbstractLoggingActor {
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class SerializedByteMessage<T> implements Serializable {
-        private static final long serialVersionUID = 1234507743872319842L;
-        private byte[] bytes;
-        private ActorRef sender;
-        private ActorRef receiver;
-    }
-
-    @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
     public static class SourceMessage implements Serializable {
-        private static final long serialVersionUID = 2432507743872319842L;
+        private static final long serialVersionUID = 3432507743872319842L;
         private SourceRef<List<Byte>> sourceRef;
         private int length;
         private ActorRef sender;
         private ActorRef receiver;
 
+    }
+
+    /////////////////
+    // Actor State //
+    /////////////////
+
+    /////////////////////
+    // Actor Lifecycle //
+    /////////////////////
+
+    ////////////////////
+    // Actor Behavior //
+    ////////////////////
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(LargeMessage.class, this::handle)
+                .match(BytesMessage.class, this::handle)
+                .match(SourceMessage.class, this::handle)
+                .matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
+                .build();
+    }
+
+    private void handle(LargeMessage<?> message) {
+        ActorRef receiver = message.getReceiver();
+        ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
+
+        try {
+            // Serializing the data
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            Kryo kryo = new Kryo();
+            Output output = new Output(byteArrayOutputStream);
+            kryo.writeClassAndObject(output, message.getMessage());
+            output.close();
+            byte[] byteArrayData = byteArrayOutputStream.toByteArray();
+
+            // Creating an Akka Streaming
+            Source<List<Byte>, NotUsed> source = Source.from(Arrays.asList(ArrayUtils.toObject(byteArrayData))).grouped(20000);
+            SourceRef<List<Byte>> sourceRef = source.runWith(StreamRefs.sourceRef(), this.context().system());
+
+            // Passing the source reference as a customized "SourceMessage"
+            receiverProxy.tell(new SourceMessage(sourceRef, byteArrayData.length, this.sender(), message.getReceiver()), this.self());
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private void handle(SourceMessage message) {
+        // Receiving the customized "SourceMessage" that we created to retrieve our SourceRef
+        message.getSourceRef()
+                .getSource()
+                .runWith(Sink.seq(), this.context().system())
+                .whenComplete
+                (
+                    (data, exception) -> {
+                        // Method to assemble the data, in this case we are creating an array of bytes
+                        int i = 0;
+                        byte[] bytes = new byte[message.getLength()];
+                        for (List<Byte> list : data) {
+                            for (Byte aByte : list) {
+                                bytes[i] = aByte;
+                                i++;
+                            }
+                        }
+
+                        try {
+                            // Deserialize the data using Kryo
+                            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+                            Kryo kryo = new Kryo();
+                            Input input = new Input(byteArrayInputStream);
+                            Object finalData = kryo.readClassAndObject(input);
+                            input.close();
+                            // Dispatch a message containing the deserialized data to the final destination
+                            message.getReceiver().tell(finalData, message.getSender());
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    });
+    }
+
+
+    private void handle(BytesMessage<?> message) {
+        // Reassemble the message content, deserialize it and/or load the content from some local location before forwarding its content.
+        message.getReceiver().tell(message.getBytes(), message.getSender());
     }
 
 }
